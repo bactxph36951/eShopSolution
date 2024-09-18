3c5721d2f7814f7f7296177517e01a44161ab7a9
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-09-18 16:25:10 +0700

    add product apis

--- eShopSolution.Application/Catalog/Products/PublicProductService.cs
+++ eShopSolution.Application/Catalog/Products/PublicProductService.cs
@@ -20,12 +20,13 @@ namespace eShopSolution.Application.Catalog.Products
             _context = context;
         }
 
-        public async Task<List<ProductViewModel>> GetAll()
+        public async Task<List<ProductViewModel>> GetAll(string languageId)
         {
             var query = from p in _context.Products
                         join pt in _context.ProductTranslations on p.Id equals pt.ProductId
                         join pic in _context.ProductInCategories on p.Id equals pic.ProductId
                         join c in _context.Categories on pic.CategoryId equals c.Id
+                        where pt.LanguageId == languageId
                         select new { p, pt, pic };
 
             var data = await query.Select(x => new ProductViewModel()
@@ -54,7 +55,7 @@ namespace eShopSolution.Application.Catalog.Products
                         join pt in _context.ProductTranslations on p.Id equals pt.ProductId
                         join pic in _context.ProductInCategories on p.Id equals pic.ProductId
                         join c in _context.Categories on pic.CategoryId equals c.Id
-
+                        where pt.LanguageId == request.LanguageId
                         select new { p, pt, pic };
 
             if (request.CategoryId.HasValue && request.CategoryId.Value > 0)
