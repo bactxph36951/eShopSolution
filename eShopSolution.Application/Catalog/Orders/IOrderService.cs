75f253b48db914098cb38beb02f24ae6705925c7
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-10-03 11:09:08 +0700

    checkout cart

---
+++ eShopSolution.Application/Catalog/Orders/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using eShopSolution.ViewModels.Catalog.Products;
+using eShopSolution.ViewModels.Sales;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Orders
+{
+    public interface IOrderService
+    {
+        Task<CheckoutRequest> GetById(int orderId, string languageId);
+    }
+}
---
+++ eShopSolution.Application/Catalog/Orders/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using eShopSolution.Application.Common;
+using eShopSolution.Data.EF;
+using eShopSolution.ViewModels.Catalog.Products;
+using eShopSolution.ViewModels.Sales;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Orders
+{
+    public class OrderService : IOrderService
+    {
+        private readonly EShopDbContext _context;
+
+        public OrderService(EShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<CheckoutRequest> GetById(int orderId, string languageId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
